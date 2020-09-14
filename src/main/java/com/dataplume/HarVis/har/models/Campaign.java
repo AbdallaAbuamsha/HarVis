0a4d963c40e401ce74b03a42d5b6b1ae9238555a
ABD\Abdalla <abdalla0abu0amsha@gmail.com>
2020-09-14 10:45:28 +0300

    Add Campaign Evolve Flow.

--- src/main/java/com/dataplume/HarVis/har/models/Campaign.java
+++ src/main/java/com/dataplume/HarVis/har/models/Campaign.java
@@ -4,16 +4,19 @@ import com.dataplume.HarVis.har.models.crawlers.FacebookCrawler;
 import com.dataplume.HarVis.har.models.crawlers.TwitterCrawler;
 import com.dataplume.HarVis.har.models.crawlers.YoutubeCrawler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Campaign {
     private Search search;
     private int searchRound;
-    private EvolveWord evolveWord;
+    private String[][] evolvedWords;
 
     public Campaign(Search search) {
         this.search = search;
         this.searchRound = 1;
-        this.evolveWord = new EvolveWord(search.getSearchKeywords());
+        this.evolvedWords = new String[search.getMaxEvolveDepth()][10];
     }
 
     public void startCrawling()
@@ -31,5 +34,25 @@ public class Campaign {
                 crawler = new FacebookCrawler(search);
                 break;
         }
+        List<Post> postsList = crawler.getData();
+        filterData(postsList);
+        List<Post> roundPostsList = null;
+        for(searchRound = 0 ; searchRound  < search.getMaxEvolveDepth(); searchRound++) {
+            evolvedWords[searchRound] = (searchRound == 0)? getMostFrequentWords(postsList) : getMostFrequentWords(postsList);
+            roundPostsList = new ArrayList<>();
+            for(int i = 0 ; i < 10 ; i++)
+            {
+                search.setSearchKeywords(evolvedWords[searchRound][i]);
+                roundPostsList.addAll(crawler.getData());
+            }
+            postsList.addAll(roundPostsList);
+        }
+    }
+
+    private void filterData(List<Post> postsList) {
+    }
+
+    private String[] getMostFrequentWords(List<Post> postList) {
+        return null;
     }
 }
